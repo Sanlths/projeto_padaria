3bc911d661c29cad299019b6c58e66d101e4214f
San <lthstecnologia@proton.me>
2026-08-28 16:02:04 -0300

    Create dados.js

---
+++ dados.js
@@ -0,0 +1,86 @@
+export const categorias = [
+  'Todos',
+  'Pães',
+  'Doces',
+  'Salgados',
+  'Bebidas',
+]
+
+export const produtos = [
+  {
+    id: 'pao-frances',
+    nome: 'Pão Francês',
+    categoria: 'Pães',
+    preco: 2,
+    descricao:
+      'Casquinha crocante, miolo leve e fornada fresca ao longo do dia.',
+    destaque: 'Fresco hoje',
+    imagem: 'img/pao-frances.svg',
+  },
+  {
+    id: 'bolo-cenoura',
+    nome: 'Bolo de Cenoura',
+    categoria: 'Doces',
+    preco: 7.5,
+    descricao:
+      'Massa macia de cenoura com cobertura cremosa de chocolate.',
+    destaque: 'Clássico da casa',
+    imagem: 'img/bolo-cenoura.svg',
+  },
+  {
+    id: 'croissant',
+    nome: 'Croissant',
+    categoria: 'Pães',
+    preco: 6,
+    descricao:
+      'Massa folhada amanteigada, leve e assada até ficar dourada.',
+    destaque: 'Mais pedido',
+    imagem: 'img/croissant.svg',
+  },
+  {
+    id: 'sonho-creme',
+    nome: 'Sonho com Creme',
+    categoria: 'Doces',
+    preco: 5,
+    descricao:
+      'Massa delicada, recheio cremoso e finalização com açúcar.',
+    destaque: 'Doce favorito',
+    imagem: 'img/sonho.svg',
+  },
+  {
+    id: 'pao-queijo',
+    nome: 'Pão de Queijo',
+    categoria: 'Pães',
+    preco: 4,
+    descricao:
+      'Dourado por fora, macio por dentro e com sabor marcante de queijo.',
+    destaque: 'Sai quentinho',
+    imagem: 'img/pao-queijo.svg',
+  },
+  {
+    id: 'torta-frango',
+    nome: 'Torta de Frango',
+    categoria: 'Salgados',
+    preco: 8.5,
+    descricao:
+      'Massa leve com recheio cremoso de frango e temperos suaves.',
+    destaque: 'Boa para o almoço',
+    imagem: 'img/torta-frango.svg',
+  },
+  {
+    id: 'cafe-leite',
+    nome: 'Café com Leite',
+    categoria: 'Bebidas',
+    preco: 3,
+    descricao:
+      'Café passado na hora com leite quente e textura aveludada.',
+    destaque: 'Combina com tudo',
+    imagem: 'img/cafe-leite.svg',
+  },
+]
+
+export const produtosDestaque = [
+  'croissant',
+  'pao-queijo',
+  'cafe-leite',
+]
